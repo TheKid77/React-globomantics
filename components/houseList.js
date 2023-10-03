52019d9ab2cc811c6eaf3e4147b9a5e3bbc838a4
TheKid77 <andymcc77@blueyonder.co.uk>
2023-10-03 09:39:36 +0100

    custom hook added

--- components/houseList.js
+++ components/houseList.js
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from "react";
+import useHouses from "@/hooks/useHouses";
 import HouseRow from "./houseRow";
 
 const HouseList = ({selectHouse}) => {
-  const [houses, setHouses] = useState([]);
 
-  useEffect(() => {
-    const fetchHouses = async () => {
-      const response = await fetch("api/houses");
-      const houses = await response.json();
-      setHouses(houses);
-    };
-    fetchHouses();
-  },[]);
-
+  const {houses, setHouses} = useHouses();
   const addHouse = () => {
     setHouses([
       ...houses,
